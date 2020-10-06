31c7afdbda8811a63eea7164ae3ff8b37074d901
Nguyen <phingu@08f8bc643a64.ant.amazon.com>
2020-10-06 10:08:17 -0700

    s3>emr>s3

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from datetime import datetime, timedelta 
 from airflow import DAG
@@ -6,14 +7,27 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator 
 from airflow.hooks.S3_hook import S3Hook
 from airflow.operators import PythonOperator
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
 
 # local config
 unload_user_purchase ='/scripts/sql/filter_unload_user_purchase.sql'
 temp_filtered_user_purchase = '/temp/temp_filtered_user_purchase.csv'
+movie_review_local = '/data/movie_review/movie_review.csv' 
+
+movie_clean_emr_steps = './dags/scripts/emr/clean_movie_review.json'
+movie_text_classification_script = './dags/scripts/spark/random_text_classification.py'
 
 # remote config
 BUCKET_NAME = 'etl-data-store'
 temp_filtered_user_purchase_key= 'user_purchase/stage/{{ ds }}/temp_filtered_user_purchase.csv'
+movie_review_load = 'movie_review/load/movie.csv'
+
+EMR_ID = 'j-2DCIUIOK8CYQF'
+movie_review_load_folder = 'movie_review/load/'
+movie_review_stage = 'movie_review/stage/'
+text_classifier_script = 'scripts/random_text_classifier.py'
+
 
 # helper function(s)
 def _local_to_s3(filename, key, bucket_name=BUCKET_NAME):
@@ -40,37 +54,88 @@ default_args = {
 
 dag = DAG("user_behaviour", default_args=default_args, schedule_interval="0 0 * * *", max_active_runs=1)
 
-end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipeline', dag=dag)
+# end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipeline', dag=dag)
 
-pg_unload = PostgresOperator(
-    dag=dag,
-    task_id='pg_unload',
-    sql=unload_user_purchase,
-    postgres_conn_id='postgres_default',
-    params={'temp_filtered_user_purchase': temp_filtered_user_purchase},
-    depends_on_past=True,
-    wait_for_downstream=True
-)
+# pg_unload = PostgresOperator(
+#     dag=dag,
+#     task_id='pg_unload',
+#     sql=unload_user_purchase,
+#     postgres_conn_id='postgres_default',
+#     params={'temp_filtered_user_purchase': temp_filtered_user_purchase},
+#     depends_on_past=True,
+#     wait_for_downstream=True
+# )
 
-user_purchase_to_s3_stage = PythonOperator(
+# user_purchase_to_s3_stage = PythonOperator(
+#     dag=dag,
+#     task_id='user_purchase_to_s3_stage',
+#     python_callable=_local_to_s3,
+#     op_kwargs={
+#         'filename': temp_filtered_user_purchase,
+#         'key': temp_filtered_user_purchase_key,
+#     },
+# )
+
+# remove_local_user_purchase_file = PythonOperator(
+#     dag=dag,
+#     task_id='remove_local_user_purchase_file',
+#     python_callable=remove_local_file,
+#     op_kwargs={
+#         'filelocation': temp_filtered_user_purchase,
+#     },
+# )
+
+movie_review_to_s3_stage = PythonOperator(
     dag=dag,
-    task_id='user_purchase_to_s3_stage',
+    task_id='movie_review_to_s3_stage',
     python_callable=_local_to_s3,
     op_kwargs={
-        'filename': temp_filtered_user_purchase,
-        'key': temp_filtered_user_purchase_key,
+        'filename': movie_review_local,
+        'key': movie_review_load,
     },
 )
 
-remove_local_user_purchase_file = PythonOperator(
+move_emr_script_to_s3 = PythonOperator(
     dag=dag,
-    task_id='remove_local_user_purchase_file',
-    python_callable=remove_local_file,
+    task_id='move_emr_script_to_s3',
+    python_callable=_local_to_s3,
     op_kwargs={
-        'filelocation': temp_filtered_user_purchase,
+        'filename': movie_text_classification_script,
+        'key': 'scripts/random_text_classification.py',
     },
 )
 
-pg_unload >> user_purchase_to_s3_stage >> remove_local_user_purchase_file >> end_of_data_pipeline
+with open(movie_clean_emr_steps) as json_file:
+    emr_steps = json.load(json_file)
 
+# adding our EMR steps to an existing EMR cluster
+add_emr_steps = EmrAddStepsOperator(
+    dag=dag,
+    task_id='add_emr_steps',
+    job_flow_id=EMR_ID,
+    aws_conn_id='aws_default',
+    steps=emr_steps,
+    params={
+        'BUCKET_NAME': BUCKET_NAME,
+        'movie_review_load': movie_review_load_folder,
+        'text_classifier_script': text_classifier_script,
+        'movie_review_stage': movie_review_stage
+    },
+    depends_on_past=True
+)
+
+last_step = len(emr_steps) - 1
+
+# sensing if the last step is complete
+clean_movie_review_data = EmrStepSensor(
+    dag=dag,
+    task_id='clean_movie_review_data',
+    job_flow_id=EMR_ID,
+    step_id='{{ task_instance.xcom_pull("add_emr_steps", key="return_value")[' + str(
+        last_step) + '] }}',
+    depends_on_past=True
+)
+
+[movie_review_to_s3_stage, move_emr_script_to_s3] >> add_emr_steps >> clean_movie_review_data
+#pg_unload >> user_purchase_to_s3_stage >> remove_local_user_purchase_file >> end_of_data_pipeline 
 
